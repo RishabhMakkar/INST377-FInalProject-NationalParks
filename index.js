a7261dc468e3b80860c4a7ad127ecb18d745de90
RishabhMakkar <116852499+RishabhMakkar@users.noreply.github.com>
2025-05-17 22:38:58 -0400

    feat: Initial Commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const supabaseClient = require('@supabase/supabase-js');
+const bodyParser = require('body-parser')
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+const supabaseURL = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+
+const supabase = supabaseClient.createClient(supabaseURL, supabaseKey)
+
+app.get('/States', async(req, res) => {
+    console.log('attempting to get states')
+    const {data, error} = await supabase.from('States').select()
+    if (error) {
+        console.log(`Error:  ${error}`)
+        res.statusCode=500
+        res.send(error)
+
+    }
+    res.send(data)
+})
+
+app.get('/Parks', async(req, res) => {
+    console.log('attempting to get Parks')
+    const {data, error} = await supabase.from('Parks').select()
+    if (error) {
+        console.log(`Error:  ${error}`)
+        res.statusCode=500
+        res.send(error)
+
+    }
+    res.send(data)
+})
+    
+
+
+app.post('/Parks', async(req, res) => {
+    console.log("adding Park")
+    console.log(req.body)
+    const parkName = req.body.parkName
+    console.log(parkName)
+
+    const {data, error} = await supabase
+    .from('Parks')
+    .insert({Park_Name: parkName})
+    .select()
+    if (error) {
+        console.log(`Error: ${error}`)
+        res.statusCode=500
+        res.send(error)
+    }
+    console.log(data)
+    res.send(data);
+})
+
+/*
+app.post('/States', async(req, res) => {
+    console.log("adding state")
+    console.log(req.body)
+    const stateName = req.body.stateName
+    const stateAbbreviation = req.body.stateAbbreviation
+    console.log(stateName)
+
+    const {data, error} = await supabase
+    .from('States')
+    .insert({State_Name: stateName, State_Abbreviation: stateAbbreviation})
+    .select()
+    if (error) {
+        console.log(`Error: ${error}`)
+        res.statusCode=500
+        res.send(error)
+    }
+    console.log(data)
+    res.send(data);
+})
+    */
+
+app.listen(port, () => {
+    console.log('APP is alive on port ' + port)
+   // pick up from here tomorrow app.get('/')
+});
